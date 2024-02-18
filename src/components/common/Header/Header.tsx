012c6ece13d0d1858b8a8cbec277ebe50ca18f41
Daniil Khakhulin <dan.praeco@gmail.com>
2024-02-18 16:44:41 +0700

    feature/FRO-3 :ocean: header layout fix

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,31 +1,23 @@
 import { Link } from 'react-router-dom';
 import { Logo } from '..';
+import { HEADER_LINKS } from '@/lib/constants/navigation/HEADER_LINKS';
 
 export const Header = () => (
-  <header className='flex px-10 py-4 border-b-2'>
+  <header className='flex md:px-10 py-4 border-b-2 px-4'>
     <div className='flex font-bold items-center mr-6'>
       <Logo />
       Key Inc.
     </div>
     <nav className='flex items-center gap-6 text-sm'>
-      <Link
-        to='/registrationrequests'
-        className='transition-colors hover:text-foreground/80 text-foreground/60 active:text-current'
-      >
-        Заявки на регистрацию
-      </Link>
-      <Link
-        to='/registrationrequests'
-        className='transition-colors hover:text-foreground/80 text-foreground/60 active:text-current'
-      >
-        Ключи
-      </Link>
-      <Link
-        to='/registrationrequests'
-        className='transition-colors hover:text-foreground/80 text-foreground/60 active:text-current'
-      >
-        Заявки на регистрацию
-      </Link>
+      {HEADER_LINKS.map((link, index) => (
+        <Link
+          to={link.href}
+          className='transition-colors hover:text-foreground/80 text-foreground/60 active:text-current'
+          key={index}
+        >
+          <span>{link.text}</span>
+        </Link>
+      ))}
     </nav>
   </header>
 );
--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -2,14 +2,7 @@ import logo from '@/assets/key_logo.svg';
 import { cn } from '@/lib';
 import React from 'react';
 
-interface LogoProps extends React.ComponentPropsWithoutRef<'img'> {}
-
-// interface AddressCardSkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-// export const AddressCardSkeleton = React.forwardRef<HTMLDivElement, AddressCardSkeletonProps>(
-//   ({ className, ...props }, ref) => (
-//     <div ref={ref} className={cn('w-full', className)} {...props}>
-//     </div>
+interface LogoProps extends React.ComponentPropsWithRef<'img'> {}
 
 export const Logo = React.forwardRef<HTMLDivElement, LogoProps>(({ className }) => (
   <img src={logo} alt='logo' className={cn('w-7', className)} />
