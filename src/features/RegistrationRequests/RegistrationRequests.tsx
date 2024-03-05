8e261124730dfc1273852ed3e26191303d7afaf4
Daniil Khakhulin <dan.praeco@gmail.com>
2024-03-05 22:17:29 +0700

    feature/FRO-22 :ocean: added adaptive layout on requests page, added burger menu

--- src/features/RegistrationRequests/RegistrationRequests.tsx
+++ src/features/RegistrationRequests/RegistrationRequests.tsx
@@ -1,17 +1,27 @@
 import { Input } from '@/components/ui';
 import { RegistrationRequestCard } from './components/RegistrationRequestCard/RegistrationRequestCard';
 import { useRegistrationsRequests } from './hooks/useRegistrationsRequests';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { Pagination } from '@/components/common';
 
 export const RegistrationRequests = () => {
-  const { users, setParamsByName, params, nextPage, previousPage } = useRegistrationsRequests();
+  const { users, setParamsByName, params, nextPage, previousPage } =
+    useRegistrationsRequests();
 
   return (
     <div>
-      <div className='flex gap-5 py-4'>
-        <Select onValueChange={(value) => setParamsByName('Sorting', value)} defaultValue={params.get('Sorting') || ''}>
-          <SelectTrigger className='w-[200px]'>
+      <div className='flex gap-5 py-4 flex-wrap md:items-center justify-center md:justify-start'>
+        <Select
+          onValueChange={(value) => setParamsByName('Sorting', value)}
+          defaultValue={params.get('Sorting') || ''}
+        >
+          <SelectTrigger className='w-48'>
             <SelectValue placeholder='Сортировка' />
           </SelectTrigger>
           <SelectContent>
@@ -21,7 +31,11 @@ export const RegistrationRequests = () => {
             <SelectItem value='AgeDesc'>Сначала старше</SelectItem>
           </SelectContent>
         </Select>
-        <Input placeholder='Имя' className='w-48' onChange={(e) => setParamsByName('FullName', e.target.value)} />
+        <Input
+          placeholder='Имя'
+          className='w-48'
+          onChange={(e) => setParamsByName('FullName', e.target.value)}
+        />
       </div>
 
       <div className='flex gap-4 flex-wrap items-center justify-center'>
