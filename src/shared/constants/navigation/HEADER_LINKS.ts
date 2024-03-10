4aea5400791913c305eeef523ea050251af0c0f0
Daniil Khakhulin <dan.praeco@gmail.com>
2024-03-10 16:39:19 +0700

    feature/FRO-33 :ocean: header links update

--- src/shared/constants/navigation/HEADER_LINKS.ts
+++ src/shared/constants/navigation/HEADER_LINKS.ts
@@ -10,16 +10,16 @@ export const HEADER_LINKS: NavigationLinkInfo[] = [
     text: 'Заявки на регистрацию',
     href: ROUTES.REGISTRATIONREQUESTS,
   },
-  // {
-  //   text: 'Заявки на ключи',
-  //   href: ROUTES.ROOT,
-  // },
+  {
+    text: 'Заявки на ключи',
+    href: ROUTES.ROOT,
+  },
   {
     text: 'Ключи',
     href: ROUTES.KEYS,
   },
-  {
-    text: 'Профиль',
-    href: ROUTES.PROFILE,
-  },
+  // {
+  //   text: 'Профиль',
+  //   href: ROUTES.PROFILE,
+  // },
 ];
